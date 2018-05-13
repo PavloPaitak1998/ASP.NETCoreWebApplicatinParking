35d8e000a5d439f639616d765ec015a3134b3ad1
PavloPaitak <38991419+PavloPaitak@users.noreply.github.com>
2018-05-14 02:35:00 +0300

    Created TransactionsController

---
+++ ClassLibrary/WebApplicationParking/Controllers/TransactionsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClassLibrary;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationParking.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]/[action]")]
+    public class TransactionsController : Controller
+    {
+        Parking parking;
+
+        public TransactionsController(Parking parking) => this.parking = parking;
+
+    }
+}
