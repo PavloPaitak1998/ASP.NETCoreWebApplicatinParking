fde21fb975c1d31e5ebc77bd167a1fa06f17fd83
PavloPaitak <38991419+PavloPaitak@users.noreply.github.com>
2018-05-14 02:16:40 +0300

    Created ParkingController

---
+++ ClassLibrary/WebApplicationParking/Controllers/ParkingController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClassLibrary;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationParking.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]/[action]")]
+    public class ParkingController : Controller
+    {
+        Parking parking;
+
+        public ParkingController(Parking parking) => this.parking = parking;
+    }
+}
