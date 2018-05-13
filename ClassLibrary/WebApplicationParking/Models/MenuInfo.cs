4ea3219fa5b4b8d1fd43b2c1ccdd462c0c62f45a
PavloPaitak <38991419+PavloPaitak@users.noreply.github.com>
2018-05-14 02:09:38 +0300

    Created Web App ASP.NET Core Web API Parking

---
+++ ClassLibrary/WebApplicationParking/Models/MenuInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationParking.Models
+{
+    public class MenuInfo
+    {
+        public string AddCar { get; private set; } = "api/Parking/AddCar";
+        public string RemoveCar { get; private set; } = "api/Parking/RemoveCar/[Carid]";
+        public string RefillBalance { get; private set; } = "api/Parking/RefillBalance/[Carid]";
+        public string CarList { get; private set; } = "api/Parking/CarList";
+        public string CarInfo { get; private set; } = "api/Parking/CarInfo/[Carid]";
+        public string FreeParkingSpaces { get; private set; } = "api/Parking/FreeParkingSpaces";
+        public string BusyParkingSpaces { get; private set; } = "api/Parking/BusyParkingSpaces";
+        public string Balance { get; private set; } = "api/Transactions/Balance";
+        public string TransactionHistory { get; private set; } = "api/Transactions/TransactionHistory";
+        public string TransactionSum { get; private set; } = "api/Transactions/TransactionSum";
+        public string TransactionHistoryCar { get; private set; } = "api/Transactions/TransactionHistoryCar/[Carid]";
+        public string Menu { get; private set; } = "api/Menu";
+    }
+}
