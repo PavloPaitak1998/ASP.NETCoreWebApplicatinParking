8c257173a58bf66067458094b60234528b5df37e
PavloPaitak <38991419+PavloPaitak@users.noreply.github.com>
2018-05-14 02:36:31 +0300

    Created action method [HttpGet] TransactionHistory

--- ClassLibrary/WebApplicationParking/Controllers/TransactionsController.cs
+++ ClassLibrary/WebApplicationParking/Controllers/TransactionsController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using ClassLibrary;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace WebApplicationParking.Controllers
 {
@@ -15,6 +16,19 @@ namespace WebApplicationParking.Controllers
         Parking parking;
 
         public TransactionsController(Parking parking) => this.parking = parking;
+
+        [HttpGet]
+        public List<Transaction> TransactionHistory()
+        {
+            DateTime date = DateTime.Now;
+
+            var transact = parking.Transactions.GetRange(0, parking.Transactions.Count);
+
+            var transactions = transact.Where(t => (date - t.TransactionTime).TotalSeconds <= 60);
+
+            string str = JsonConvert.SerializeObject(transactions);
+            return JsonConvert.DeserializeObject<List<Transaction>>(str);
+        }
 
     }
 }
