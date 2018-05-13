95adfceb5ffec539a31bef36013162a8a553a359
PavloPaitak <38991419+PavloPaitak@users.noreply.github.com>
2018-05-14 02:51:51 +0300

    Changed folder name

---
+++ Parking/ClassLibrary/Transaction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class Transaction
+    {
+        public DateTime TransactionTime { get; set; }
+        public int CarId { get; set; }
+        public double Payment { get; set; }
+
+        public override string ToString()
+        {
+            return "TransactionTime: " + TransactionTime + " Car Id: " + CarId + " Payment: " + Payment;
+        }
+    }
+}
---
+++ Parking/WebApplicationParking/Controllers/TransactionsController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClassLibrary;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace WebApplicationParking.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]/[action]")]
+    public class TransactionsController : Controller
+    {
+        Parking parking;
+
+        public TransactionsController(Parking parking) => this.parking = parking;
+
+        [HttpGet]
+        public List<Transaction> TransactionHistory()
+        {
+            DateTime date = DateTime.Now;
+
+            var transact = parking.Transactions.GetRange(0, parking.Transactions.Count);
+
+            var transactions = transact.Where(t => (date - t.TransactionTime).TotalSeconds <= 60);
+
+            string str = JsonConvert.SerializeObject(transactions);
+            return JsonConvert.DeserializeObject<List<Transaction>>(str);
+        }
+
+        [HttpGet]
+        public Object TransactionSum()
+        {
+            string transactionSum = parking.ReadSumTransactions();
+
+            string[] array = transactionSum.Split(new string[] { "TransactionTime: ", "Sum: " }, StringSplitOptions.RemoveEmptyEntries);
+
+            string str = JsonConvert.SerializeObject(new { TransactionTime = array[0], Sum = array[1] });
+            return JsonConvert.DeserializeObject<Object>(str);
+        }
+        //public async Task<Object> TransactionSum()
+        //{
+        //    string transactionSum = await parking.ReadSumTransactions();
+
+        //    string[] array = transactionSum.Split(new string[] { "TransactionTime: ","Sum: "  },StringSplitOptions.RemoveEmptyEntries);
+
+        //    string str = JsonConvert.SerializeObject(new {TransactionTime=array[0],Sum=array[1] });
+        //    return JsonConvert.DeserializeObject<Object>(str);
+        //}
+
+        [HttpGet("{id}")]
+        public List<Transaction> TransactionHistoryCar(int id)
+        {
+            DateTime date = DateTime.Now;
+
+            var transactions = parking.Transactions.Where(t => t.CarId == id && (date - t.TransactionTime).TotalSeconds <= 60);
+
+            string str = JsonConvert.SerializeObject(transactions);
+            return JsonConvert.DeserializeObject<List<Transaction>>(str);
+        }
+    }
+}
